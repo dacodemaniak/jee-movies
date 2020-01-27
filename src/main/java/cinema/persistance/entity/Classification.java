cd50f121ee35e2630399fa22aaf594abcfdd565c
Aleksejs Pridannikovs <alex.demolliens@gmail.com>
2020-01-27 14:13:33 +0100

    Class Movie eddited

---
+++ src/main/java/cinema/persistance/entity/Classification.java
@@ -0,0 +1,5 @@
+package cinema.persistance.entity;
+
+public enum Classification {
+	G , PG, R13, R, NC17
+}
--- src/main/java/cinema/persistance/entity/Movie.java
+++ src/main/java/cinema/persistance/entity/Movie.java
@@ -15,6 +15,8 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
 @Entity
 @Table(name = "movies")
 public class Movie {
@@ -22,38 +24,65 @@ public class Movie {
 	@Column
 	private Integer idMovie;
 	private String title;
+	private String originalTitle;
 	private Integer year;
 	private Integer duration; //Integer peut être nulle car c'est une référence a un objet et non int qui est primitif
+	private List<String> genres;
+	private Float rating;
+	private Classification clasification;
+	private String synopsis;
 	private Person director;
+	private String format;
 	private List<Person> actors;
 	
+	
+	
+//	public Movie(String title, Integer year) {
+//		this(title, year, null);
+//	}
+
+//	public Movie(String title, Integer year, Integer duration) {
+//		this(null, title, year, duration,null);
+//	}
 
 	public Movie() {
-		this(null,null);
+		super();
 	}
 	
-	public Movie(String title, Integer year) {
-		this(title, year, null);
+	public Movie(String title, int year) {
+		this(null, title, year, null, null, null, null);
 	}
-
-	public Movie(String title, Integer year, Integer duration) {
-		this(null, title, year, duration,null);
-	}	
 	
-	public Movie(String title, Integer year, Integer duration,Person director) {
-		this(null, title, year, duration, director);
+	public Movie(String title, int year, int duration) {
+		this(null, title, year, duration, null, null, null);
+	}
+	
+	public Movie(String title, String originalTitle, int year, int duration) {
+		this(null, title, originalTitle, year, duration, null, null, null, null);
+	}
+	
+	public Movie(String title, String originalTitle, Integer year, String synopsis, String format, Person director) {
+		this(null, title, originalTitle, year, null, null, synopsis, format, director);
 	}
 
-	public Movie(Integer idMovie, String title, Integer year, Integer duration,Person director) {
+	public Movie(String title, String originalTitle, Integer year, Integer duration, String synopsis, String format, Person director) {
+		this(null, title, originalTitle, year, duration, null, synopsis, format, director);
+	}
+
+	public Movie(Integer idMovie, String title, String originalTitle, Integer year, Integer duration, Float rating, String synopsis, String format, Person director) {
 		super();
 		this.idMovie = idMovie;
 		this.title = title;
+		this.originalTitle = originalTitle;
 		this.year = year;
 		this.duration = duration;
+		this.genres = new ArrayList<>();
+		this.rating = rating;
+		this.synopsis = synopsis;
 		this.director = director;
+		this.format = format;
 		this.actors = new ArrayList<>();
 	}
-	//tout mot avec @ -> annotation
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -75,6 +104,15 @@ public class Movie {
 		this.title = title;
 	}
 
+	@Column(name = "original_title", nullable = true, length = 255)
+	public String getOriginalTitle() {
+		return originalTitle;
+	}
+
+	public void setOriginalTitle(String originalTitle) {
+		this.originalTitle = originalTitle;
+	}
+
 	@Column(nullable = false)
 	public Integer getYear() {
 		return year;
@@ -92,6 +130,15 @@ public class Movie {
 		this.duration = duration;
 	}
 	
+	@Column(name = "genres", nullable = true)
+	public List<String> getGenres() {
+		return genres;
+	}
+
+	public void setGenres(List<String> genres) {
+		this.genres = genres;
+	}
+
 	@ManyToOne
 	@JoinColumn(name="id_director",nullable=true)
 	public Person getDirector() {
@@ -102,6 +149,49 @@ public class Movie {
 		this.director = director;
 	}
 	
+	@Column(name = "rating", nullable = true)
+	public Float getRating() {
+		return rating;
+	}
+
+	public void setRating(Float rating) {
+		this.rating = rating;
+	}
+	
+	@Column(name = "clasification", nullable = true)
+	public Classification getClasification() {
+		return clasification;
+	}
+
+
+
+	
+	public void setClasification(Classification clasification) {
+		this.clasification = clasification;
+	}
+	
+
+
+
+	@Column(name = "resume", nullable = true, length = 65535)
+	public String getSynopsis() {
+		return synopsis;
+	}
+
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+	
+	public String getFormat() {
+		return format;
+	}
+
+	
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+
 	@ManyToMany //(fetch = FetchType.EAGER)
 	@JoinTable(name="act",
     joinColumns=
